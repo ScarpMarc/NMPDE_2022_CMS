81eb94ad1c87f974562b3ff5d758bdd386845cec
Marco Scarpelli <underscore.marczero@gmail.com>
2023-02-28 19:44:15 +0100

    Parametrised programme

--- src/Problem.cpp
+++ src/Problem.cpp
@@ -1,7 +1,7 @@
-#include "Stokes.hpp"
+#include "Problem.hpp"
+#include "PreconditionBlockTriangular.hpp"
 
-void
-Stokes::setup()
+void Problem::setup()
 {
   // Create the mesh.
   {
@@ -14,14 +14,13 @@ Stokes::setup()
 
     /*const std::string mesh_file_name =
       "../mesh/mesh-step-" + std::to_string(N + 1) + ".msh";*/
-    const std::string mesh_file_name = mesh_file_name_glob;
 
-    std::ifstream grid_in_file(mesh_file_name);
+    std::ifstream grid_in_file(simulation_settings.file_name);
     grid_in.read_msh(grid_in_file);
 
     GridTools::partition_triangulation(mpi_size, mesh_serial);
     const auto construction_data = TriangulationDescription::Utilities::
-      create_description_from_triangulation(mesh_serial, MPI_COMM_WORLD);
+        create_description_from_triangulation(mesh_serial, MPI_COMM_WORLD);
     mesh.create_triangulation(construction_data);
 
     pcout << "  Number of elements = " << mesh.n_global_active_cells()
@@ -81,14 +80,14 @@ Stokes::setup()
     // system (velocity and pressure), we will also need those for the
     // individual velocity and pressure blocks.
     std::vector<types::global_dof_index> dofs_per_block =
-      DoFTools::count_dofs_per_fe_block(dof_handler, block_component);
+        DoFTools::count_dofs_per_fe_block(dof_handler, block_component);
     const unsigned int n_u = dofs_per_block[0];
     const unsigned int n_p = dofs_per_block[1];
 
     block_owned_dofs.resize(2);
     block_relevant_dofs.resize(2);
-    block_owned_dofs[0]    = locally_owned_dofs.get_view(0, n_u);
-    block_owned_dofs[1]    = locally_owned_dofs.get_view(n_u, n_u + n_p);
+    block_owned_dofs[0] = locally_owned_dofs.get_view(0, n_u);
+    block_owned_dofs[1] = locally_owned_dofs.get_view(n_u, n_u + n_p);
     block_relevant_dofs[0] = locally_relevant_dofs.get_view(0, n_u);
     block_relevant_dofs[1] = locally_relevant_dofs.get_view(n_u, n_u + n_p);
 
@@ -114,15 +113,15 @@ Stokes::setup()
     // the sparsity pattern can be built accordingly.
     Table<2, DoFTools::Coupling> coupling(dim + 1, dim + 1);
     for (unsigned int c = 0; c < dim + 1; ++c)
+    {
+      for (unsigned int d = 0; d < dim + 1; ++d)
       {
-        for (unsigned int d = 0; d < dim + 1; ++d)
-          {
-            if (c == dim && d == dim) // pressure-pressure term
-              coupling[c][d] = DoFTools::none;
-            else // other combinations
-              coupling[c][d] = DoFTools::always;
-          }
+        if (c == dim && d == dim) // pressure-pressure term
+          coupling[c][d] = DoFTools::none;
+        else // other combinations
+          coupling[c][d] = DoFTools::always;
       }
+    }
 
     TrilinosWrappers::BlockSparsityPattern sparsity(block_owned_dofs,
                                                     MPI_COMM_WORLD);
@@ -131,17 +130,17 @@ Stokes::setup()
 
     // We also build a sparsity pattern for the pressure mass matrix.
     for (unsigned int c = 0; c < dim + 1; ++c)
+    {
+      for (unsigned int d = 0; d < dim + 1; ++d)
       {
-        for (unsigned int d = 0; d < dim + 1; ++d)
-          {
-            if (c == dim && d == dim) // pressure-pressure term
-              coupling[c][d] = DoFTools::always;
-            else // other combinations
-              coupling[c][d] = DoFTools::none;
-          }
+        if (c == dim && d == dim) // pressure-pressure term
+          coupling[c][d] = DoFTools::always;
+        else // other combinations
+          coupling[c][d] = DoFTools::none;
       }
+    }
     TrilinosWrappers::BlockSparsityPattern sparsity_pressure_mass(
-      block_owned_dofs, MPI_COMM_WORLD);
+        block_owned_dofs, MPI_COMM_WORLD);
     DoFTools::make_sparsity_pattern(dof_handler,
                                     coupling,
                                     sparsity_pressure_mass);
@@ -159,124 +158,123 @@ Stokes::setup()
   }
 }
 
-void
-Stokes::assemble()
+void Problem::assemble()
 {
   pcout << "===============================================" << std::endl;
   pcout << "Assembling the system" << std::endl;
 
   const unsigned int dofs_per_cell = fe->dofs_per_cell;
-  const unsigned int n_q           = quadrature->size();
-  const unsigned int n_q_face      = quadrature_face->size();
+  const unsigned int n_q = quadrature->size();
+  const unsigned int n_q_face = quadrature_face->size();
 
-  FEValues<dim>     fe_values(*fe,
+  FEValues<dim> fe_values(*fe,
                           *quadrature,
                           update_values | update_gradients |
-                            update_quadrature_points | update_JxW_values);
+                              update_quadrature_points | update_JxW_values);
   FEFaceValues<dim> fe_face_values(*fe,
                                    *quadrature_face,
                                    update_values | update_normal_vectors |
-                                     update_JxW_values);
+                                       update_JxW_values);
 
   FullMatrix<double> cell_matrix(dofs_per_cell, dofs_per_cell);
   FullMatrix<double> cell_pressure_mass_matrix(dofs_per_cell, dofs_per_cell);
-  Vector<double>     cell_rhs(dofs_per_cell);
+  Vector<double> cell_rhs(dofs_per_cell);
 
   std::vector<types::global_dof_index> dof_indices(dofs_per_cell);
 
   system_matrix = 0.0;
-  system_rhs    = 0.0;
+  system_rhs = 0.0;
   pressure_mass = 0.0;
 
   FEValuesExtractors::Vector velocity(0);
   FEValuesExtractors::Scalar pressure(dim);
 
   for (const auto &cell : dof_handler.active_cell_iterators())
+  {
+    if (!cell->is_locally_owned())
+      continue;
+
+    fe_values.reinit(cell);
+
+    cell_matrix = 0.0;
+    cell_rhs = 0.0;
+    cell_pressure_mass_matrix = 0.0;
+
+    for (unsigned int q = 0; q < n_q; ++q)
     {
-      if (!cell->is_locally_owned())
-        continue;
+      Vector<double> forcing_term_loc(dim);
+      forcing_term.vector_value(fe_values.quadrature_point(q),
+                                forcing_term_loc);
+      Tensor<1, dim> forcing_term_tensor;
+      for (unsigned int d = 0; d < dim; ++d)
+        forcing_term_tensor[d] = forcing_term_loc[d];
 
-      fe_values.reinit(cell);
-
-      cell_matrix               = 0.0;
-      cell_rhs                  = 0.0;
-      cell_pressure_mass_matrix = 0.0;
-
-      for (unsigned int q = 0; q < n_q; ++q)
+      for (unsigned int i = 0; i < dofs_per_cell; ++i)
+      {
+        for (unsigned int j = 0; j < dofs_per_cell; ++j)
         {
-          Vector<double> forcing_term_loc(dim);
-          forcing_term.vector_value(fe_values.quadrature_point(q),
-                                    forcing_term_loc);
-          Tensor<1, dim> forcing_term_tensor;
-          for (unsigned int d = 0; d < dim; ++d)
-            forcing_term_tensor[d] = forcing_term_loc[d];
+          // Viscosity term.
+          cell_matrix(i, j) +=
+              simulation_settings.coeff_nu *
+              scalar_product(fe_values[velocity].gradient(i, q),
+                             fe_values[velocity].gradient(j, q)) *
+              fe_values.JxW(q);
 
-          for (unsigned int i = 0; i < dofs_per_cell; ++i)
-            {
-              for (unsigned int j = 0; j < dofs_per_cell; ++j)
-                {
-                  // Viscosity term.
-                  cell_matrix(i, j) +=
-                    nu *
-                    scalar_product(fe_values[velocity].gradient(i, q),
-                                   fe_values[velocity].gradient(j, q)) *
-                    fe_values.JxW(q);
+          // Pressure term in the momentum equation.
+          cell_matrix(i, j) -= fe_values[velocity].divergence(i, q) *
+                               fe_values[pressure].value(j, q) *
+                               fe_values.JxW(q);
 
-                  // Pressure term in the momentum equation.
-                  cell_matrix(i, j) -= fe_values[velocity].divergence(i, q) *
-                                       fe_values[pressure].value(j, q) *
-                                       fe_values.JxW(q);
+          // Pressure term in the continuity equation.
+          cell_matrix(i, j) -= fe_values[velocity].divergence(j, q) *
+                               fe_values[pressure].value(i, q) *
+                               fe_values.JxW(q);
 
-                  // Pressure term in the continuity equation.
-                  cell_matrix(i, j) -= fe_values[velocity].divergence(j, q) *
-                                       fe_values[pressure].value(i, q) *
-                                       fe_values.JxW(q);
-
-                  // Pressure mass matrix.
-                  cell_pressure_mass_matrix(i, j) +=
-                    fe_values[pressure].value(i, q) *
-                    fe_values[pressure].value(j, q) / nu * fe_values.JxW(q);
-                }
-
-              // Forcing term.
-              cell_rhs(i) += scalar_product(forcing_term_tensor,
-                                            fe_values[velocity].value(i, q)) *
-                             fe_values.JxW(q);
-            }
+          // Pressure mass matrix.
+          cell_pressure_mass_matrix(i, j) +=
+              fe_values[pressure].value(i, q) *
+              fe_values[pressure].value(j, q) / simulation_settings.coeff_nu * fe_values.JxW(q);
         }
 
-      // Boundary integral for Neumann BCs.f
-      if (cell->at_boundary())
-        {
-          for (unsigned int f = 0; f < cell->n_faces(); ++f)
-            {
-              if (cell->face(f)->at_boundary() &&
-                  cell->face(f)->boundary_id() == 2)
-                {
-                  fe_face_values.reinit(cell, f);
-
-                  for (unsigned int q = 0; q < n_q_face; ++q)
-                    {
-                      for (unsigned int i = 0; i < dofs_per_cell; ++i)
-                        {
-                          cell_rhs(i) +=
-                            -p_out *
-                            scalar_product(fe_face_values.normal_vector(q),
-                                           fe_face_values[velocity].value(i,
-                                                                          q)) *
-                            fe_face_values.JxW(q);
-                        }
-                    }
-                }
-            }
-        }
-
-      cell->get_dof_indices(dof_indices);
-
-      system_matrix.add(dof_indices, cell_matrix);
-      system_rhs.add(dof_indices, cell_rhs);
-      pressure_mass.add(dof_indices, cell_pressure_mass_matrix);
+        // Forcing term.
+        cell_rhs(i) += scalar_product(forcing_term_tensor,
+                                      fe_values[velocity].value(i, q)) *
+                       fe_values.JxW(q);
+      }
     }
+
+    // Boundary integral for Neumann BCs.f
+    if (cell->at_boundary())
+    {
+      for (unsigned int f = 0; f < cell->n_faces(); ++f)
+      {
+        if (cell->face(f)->at_boundary() &&
+            cell->face(f)->boundary_id() == 2)
+        {
+          fe_face_values.reinit(cell, f);
+
+          for (unsigned int q = 0; q < n_q_face; ++q)
+          {
+            for (unsigned int i = 0; i < dofs_per_cell; ++i)
+            {
+              cell_rhs(i) +=
+                  -simulation_settings.outlet_pressure *
+                  scalar_product(fe_face_values.normal_vector(q),
+                                 fe_face_values[velocity].value(i,
+                                                                q)) *
+                  fe_face_values.JxW(q);
+            }
+          }
+        }
+      }
+    }
+
+    cell->get_dof_indices(dof_indices);
+
+    system_matrix.add(dof_indices, cell_matrix);
+    system_rhs.add(dof_indices, cell_rhs);
+    pressure_mass.add(dof_indices, cell_pressure_mass_matrix);
+  }
 
   system_matrix.compress(VectorOperation::add);
   system_rhs.compress(VectorOperation::add);
@@ -284,7 +282,7 @@ Stokes::assemble()
 
   // Dirichlet boundary conditions.
   {
-    std::map<types::global_dof_index, double>           boundary_values;
+    std::map<types::global_dof_index, double> boundary_values;
     std::map<types::boundary_id, const Function<dim> *> boundary_functions;
 
     // We interpolate first the inlet velocity condition alone, then the wall
@@ -295,7 +293,7 @@ Stokes::assemble()
                                              boundary_functions,
                                              boundary_values,
                                              ComponentMask(
-                                               {true, true, true, false}));
+                                                 {true, true, true, false}));
 
     boundary_functions.clear();
     Functions::ZeroFunction<dim> zero_function(dim + 1);
@@ -304,21 +302,18 @@ Stokes::assemble()
                                              boundary_functions,
                                              boundary_values,
                                              ComponentMask(
-                                               {true, true, true, false}));
+                                                 {true, true, true, false}));
 
     MatrixTools::apply_boundary_values(
-      boundary_values, system_matrix, solution, system_rhs, false);
+        boundary_values, system_matrix, solution, system_rhs, false);
   }
 }
 
-void
-Stokes::solve()
+void Problem::solve()
 {
   pcout << "===============================================" << std::endl;
 
-  //deallog.attach(std::cout);
-
-  SolverControl solver_control(20000, 1e-6 * system_rhs.l2_norm()/*, true*/);
+  SolverControl solver_control(simulation_settings.max_solver_iteration_amt, simulation_settings.desired_solver_precision * system_rhs.l2_norm() /*, true*/);
 
   SolverGMRES<TrilinosWrappers::MPI::BlockVector> solver(solver_control);
 
@@ -339,18 +334,17 @@ Stokes::solve()
   solution = solution_owned;
 }
 
-void
-Stokes::output()
+void Problem::output()
 {
   pcout << "===============================================" << std::endl;
 
   DataOut<dim> data_out;
 
   std::vector<DataComponentInterpretation::DataComponentInterpretation>
-    data_component_interpretation(
-      dim, DataComponentInterpretation::component_is_part_of_vector);
+      data_component_interpretation(
+          dim, DataComponentInterpretation::component_is_part_of_vector);
   data_component_interpretation.push_back(
-    DataComponentInterpretation::component_is_scalar);
+      DataComponentInterpretation::component_is_scalar);
   std::vector<std::string> names = {"velocity",
                                     "velocity",
                                     "velocity",
@@ -368,18 +362,18 @@ Stokes::output()
 
   data_out.build_patches();
 
-  const std::string output_file_name = "output-" + std::to_string(N);
+  const std::string output_file_name = "ptero_out";
 
   DataOutBase::DataOutFilter data_filter(
-    DataOutBase::DataOutFilterFlags(/*filter_duplicate_vertices = */ false,
-                                    /*xdmf_hdf5_output = */ true));
+      DataOutBase::DataOutFilterFlags(/*filter_duplicate_vertices = */ false,
+                                      /*xdmf_hdf5_output = */ true));
   data_out.write_filtered_data(data_filter);
   data_out.write_hdf5_parallel(data_filter,
                                output_file_name + ".h5",
                                MPI_COMM_WORLD);
 
   std::vector<XDMFEntry> xdmf_entries({data_out.create_xdmf_entry(
-    data_filter, output_file_name + ".h5", 0, MPI_COMM_WORLD)});
+      data_filter, output_file_name + ".h5", 0, MPI_COMM_WORLD)});
   data_out.write_xdmf_file(xdmf_entries,
                            output_file_name + ".xdmf",
                            MPI_COMM_WORLD);
---
+++ src/Problem.hpp
@@ -0,0 +1,249 @@
+#ifndef STOKES_HPP
+#define STOKES_HPP
+
+#include <deal.II/base/conditional_ostream.h>
+#include <deal.II/base/quadrature_lib.h>
+
+#include <deal.II/distributed/fully_distributed_tria.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_renumbering.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_simplex_p.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/fe_values_extractors.h>
+#include <deal.II/fe/mapping_fe.h>
+
+#include <deal.II/grid/grid_in.h>
+
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/solver_gmres.h>
+#include <deal.II/lac/trilinos_block_sparse_matrix.h>
+#include <deal.II/lac/trilinos_parallel_block_vector.h>
+#include <deal.II/lac/trilinos_precondition.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/numerics/vector_tools.h>
+
+#include <fstream>
+#include <iostream>
+
+#include <string>
+
+#include "SimulationSettings.hpp"
+#include "PreconditionBlockTriangular.hpp"
+
+using namespace dealii;
+
+// Class implementing a solver for the Stokes problem.
+class Problem
+{
+public:
+  // Physical dimension (1D, 2D, 3D)
+  static constexpr unsigned int dim = 3;
+
+  // Function for the forcing term.
+  class ForcingTerm : public Function<dim>
+  {
+  public:
+    virtual void
+    vector_value(const Point<dim> & /*p*/,
+                 Vector<double> &values) const override
+    {
+      for (unsigned int i = 0; i < dim - 1; ++i)
+        values[i] = 0.0;
+
+      values[dim - 1] = -g;
+    }
+
+    virtual double
+    value(const Point<dim> & /*p*/,
+          const unsigned int component = 0) const override
+    {
+      if (component == dim - 1)
+        return -g;
+      else
+        return 0.0;
+    }
+
+  protected:
+    const double g = 0.0;
+  };
+
+  // Function for inlet velocity. This actually returns an object with four
+  // components (one for each velocity component, and one for the pressure), but
+  // then only the first three are really used (see the component mask when
+  // applying boundary conditions at the end of assembly). If we only return
+  // three components, however, we may get an error message due to this function
+  // being incompatible with the finite element space.
+  class InletVelocity : public Function<dim>
+  {
+  public:
+    InletVelocity(const SimulationSettings &simulation_settings_)
+        : Function<dim>(dim + 1), components(simulation_settings_.inlet_velocity)
+    {
+    }
+
+    virtual void
+    vector_value(const Point<dim> &p, Vector<double> &values) const override
+    {
+
+      for (unsigned int i = 0; i < dim; ++i)
+        values[i] = components[i];
+
+      values[3] = 0.0;
+    }
+
+    virtual double
+    value(const Point<dim> &p, const unsigned int component = 0) const override
+    {
+      if (component == 3)
+        return 0.0;
+      else
+        return components[component];
+    }
+
+  protected:
+    const array<double, 3> components;
+  };
+
+  // Since we're working with block matrices, we need to make our own
+  // preconditioner class. A preconditioner class can be any class that exposes
+  // a vmult method that applies the inverse of the preconditioner.
+
+  // Identity preconditioner.
+  class PreconditionIdentity
+  {
+  public:
+    // Application of the preconditioner: we just copy the input vector (src)
+    // into the output vector (dst).
+    void
+    vmult(TrilinosWrappers::MPI::BlockVector &dst,
+          const TrilinosWrappers::MPI::BlockVector &src) const
+    {
+      dst = src;
+    }
+
+  protected:
+  };
+
+protected:
+  // Velocity stiffness matrix.
+  const TrilinosWrappers::SparseMatrix *velocity_stiffness;
+
+  // Preconditioner used for the velocity block.
+  TrilinosWrappers::PreconditionILU preconditioner_velocity;
+
+  // Pressure mass matrix.
+  const TrilinosWrappers::SparseMatrix *pressure_mass;
+
+  // Preconditioner used for the pressure block.
+  TrilinosWrappers::PreconditionILU preconditioner_pressure;
+};
+
+// Constructor.
+Problem(const SimulationSettings &simulation_settings)
+    : mpi_size(Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD)), mpi_rank(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)), pcout(std::cout, mpi_rank == 0), simulation_settings(simulation_settings), inlet_velocity(simulation_settings.inlet_velocity) /*file_name(SimulationSettings.file_name), degree_velocity(SimulationSettings.degree_velocity), degree_pressure(SimulationSettings.degree_pressure), nu(SimulationSettings.coeff_nu), p_out(SimulationSettings.outlet_pressure), */ mesh(MPI_COMM_WORLD)
+{
+}
+
+// Setup system.
+void setup();
+
+// Assemble system. We also assemble the pressure mass matrix (needed for the
+// preconditioner).
+void assemble();
+
+// Solve system.
+void solve();
+
+// Output results.
+void output();
+
+protected:
+// MPI parallel. /////////////////////////////////////////////////////////////
+const std::string file_name;
+
+// Number of MPI processes.
+const unsigned int mpi_size;
+
+// This MPI process.
+const unsigned int mpi_rank;
+
+// Parallel output stream.
+ConditionalOStream pcout;
+
+// Problem definition. ///////////////////////////////////////////////////////
+SimulationSettings simulation_settings;
+/*
+// Kinematic viscosity [m2/s].
+const double nu;
+
+// Outlet pressure [Pa].
+const double p_out;
+*/
+
+// Forcing term.
+ForcingTerm forcing_term;
+
+// Inlet velocity.
+InletVelocity inlet_velocity;
+
+// Discretization. ///////////////////////////////////////////////////////////
+
+// Polynomial degree used for velocity.
+const unsigned int degree_velocity;
+
+// Polynomial degree used for pressure.
+const unsigned int degree_pressure;
+
+// Mesh.
+parallel::fullydistributed::Triangulation<dim> mesh;
+
+// Finite element space.
+std::unique_ptr<FiniteElement<dim>> fe;
+
+// Quadrature formula.
+std::unique_ptr<Quadrature<dim>> quadrature;
+
+// Quadrature formula for face integrals.
+std::unique_ptr<Quadrature<dim - 1>> quadrature_face;
+
+// DoF handler.
+DoFHandler<dim> dof_handler;
+
+// DoFs owned by current process.
+IndexSet locally_owned_dofs;
+
+// DoFs owned by current process in the velocity and pressure blocks.
+std::vector<IndexSet> block_owned_dofs;
+
+// DoFs relevant to the current process (including ghost DoFs).
+IndexSet locally_relevant_dofs;
+
+// DoFs relevant to current process in the velocity and pressure blocks.
+std::vector<IndexSet> block_relevant_dofs;
+
+// System matrix.
+TrilinosWrappers::BlockSparseMatrix system_matrix;
+
+// Pressure mass matrix, needed for preconditioning. We use a block matrix for
+// convenience, but in practice we only look at the pressure-pressure block.
+TrilinosWrappers::BlockSparseMatrix pressure_mass;
+
+// Right-hand side vector in the linear system.
+TrilinosWrappers::MPI::BlockVector system_rhs;
+
+// System solution (without ghost elements).
+TrilinosWrappers::MPI::BlockVector solution_owned;
+
+// System solution (including ghost elements).
+TrilinosWrappers::MPI::BlockVector solution;
+}
+;
+
+#endif
