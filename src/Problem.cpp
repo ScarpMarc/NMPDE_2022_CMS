a990448204e9ca5ddced3ea1d17b578af3a5a965
Marco Scarpelli <underscore.marczero@gmail.com>
2023-07-17 12:57:08 +0000

    Revert "Started implementing Stokes only"
    This reverts commit aa59c9f4e663500c02505278f23c238874d2c587.

--- src/Problem.cpp
+++ src/Problem.cpp
@@ -166,206 +166,6 @@ void Problem::setup()
 /************************************************************************/
 /************************************************************************/
 /************************************************************************/
-void Problem::assemble_stokes_only()
-{
-  pcout << "===============================================" << std::endl;
-  pcout << "Assembling the system" << std::endl;
-
-  const unsigned int dofs_per_cell = fe->dofs_per_cell;
-  const unsigned int n_q = quadrature->size();
-  const unsigned int n_q_face = quadrature_face->size();
-
-  FEValues<dim> fe_values(*fe,
-                          *quadrature,
-                          update_values | update_gradients |
-                              update_quadrature_points | update_JxW_values);
-  FEFaceValues<dim> fe_face_values(*fe,
-                                   *quadrature_face,
-                                   update_values | update_normal_vectors |
-                                       update_JxW_values);
-
-  FullMatrix<double> cell_matrix(dofs_per_cell, dofs_per_cell);
-  FullMatrix<double> cell_pressure_mass_matrix(dofs_per_cell, dofs_per_cell);
-  Vector<double> cell_rhs(dofs_per_cell);
-
-  std::vector<types::global_dof_index> dof_indices(dofs_per_cell);
-
-  jacobian_matrix = 0.0;
-  residual_vector = 0.0;
-  // This one just for the construction of the preconditioner
-  // (Should be built at every step of the Newton method)
-  pressure_mass = 0.0;
-
-  // We use these vectors to store the old solution (i.e. at previous Newton
-  // iteration) and its gradient on quadrature nodes of the current cell:
-
-  // Tensor of rank 1 to store the local velocity solution at previous Newton step
-  //(vectorial field)
-  std::vector<Tensor<1, dim>> velocity_loc(n_q);
-  // Tensor of rank 2 to store the local velocity solution at previous Newton step
-  //(the grad of a vectorial field -> jacobian)
-  std::vector<Tensor<2, dim>> velocity_gradient_loc(n_q);
-
-  // Double to store the local pressure solution at previous Newton step
-  std::vector<double> pressure_loc(n_q);
-
-  FEValuesExtractors::Vector velocity(0);
-  FEValuesExtractors::Scalar pressure(dim);
-
-  for (const auto &cell : dof_handler.active_cell_iterators())
-  {
-    if (!cell->is_locally_owned())
-      continue;
-
-    fe_values.reinit(cell);
-
-    cell_matrix = 0.0;
-    cell_rhs = 0.0;
-    cell_pressure_mass_matrix = 0.0;
-
-    // We need to compute the Jacobian matrix and the residual for current
-    // cell. This requires knowing the value and the gradient of u^{(k)}
-    // (stored inside solution) on the quadrature nodes of the current
-    // cell. This can be accomplished through
-    // FEValues::get_function_values and FEValues::get_function_gradients.
-    fe_values[velocity].get_function_values(solution, velocity_loc);
-    fe_values[velocity].get_function_gradients(solution, velocity_gradient_loc);
-    fe_values[pressure].get_function_values(solution, pressure_loc);
-
-    for (unsigned int q = 0; q < n_q; ++q)
-    {
-      Vector<double> forcing_term_loc(dim);
-      forcing_term.vector_value(fe_values.quadrature_point(q),
-                                forcing_term_loc);
-      Tensor<1, dim> forcing_term_tensor;
-      for (unsigned int d = 0; d < dim; ++d)
-        forcing_term_tensor[d] = forcing_term_loc[d];
-
-      for (unsigned int i = 0; i < dofs_per_cell; ++i)
-      {
-        for (unsigned int j = 0; j < dofs_per_cell; ++j)
-        {
-          // Viscosity term.
-          cell_matrix(i, j) +=
-              simulation_settings.coeff_nu *
-              scalar_product(fe_values[velocity].gradient(j, q),
-                             fe_values[velocity].gradient(i, q)) *
-              fe_values.JxW(q);
-
-          // Convective term
-          /*cell_matrix(i, j) +=
-              (velocity_gradient_loc[q] *
-                   fe_values[velocity].value(j, q) *
-                   fe_values[velocity].value(i, q) +
-               fe_values[velocity].gradient(j, q) *
-                   velocity_loc[q] *
-                   fe_values[velocity].value(i, q)) *
-              fe_values.JxW(q);*/
-
-          // Pressure term in the momentum equation.
-          cell_matrix(i, j) -= fe_values[velocity].divergence(j, q) *
-                               fe_values[pressure].value(i, q) *
-                               fe_values.JxW(q);
-
-          // Pressure term in the continuity equation.
-          cell_matrix(i, j) -= fe_values[velocity].divergence(i, q) *
-                               fe_values[pressure].value(j, q) *
-                               fe_values.JxW(q);
-
-          // Term given by the special-case preconditioer
-          cell_matrix(i, j) -= gamma *
-                               fe_values[velocity].divergence(j, q) *
-                               fe_values[velocity].divergence(i, q) *
-                               fe_values.JxW(q);
-
-          // Pressure mass matrix.
-          cell_pressure_mass_matrix(i, j) +=
-              fe_values[pressure].value(i, q) *
-              fe_values[pressure].value(j, q) / simulation_settings.coeff_nu * fe_values.JxW(q);
-        }
-
-        // Get the divergence of the previous
-
-        double velocity_divergence_loc =
-            trace(velocity_gradient_loc[q]);
-
-        // Forcing term.
-
-        cell_rhs(i) +=
-            scalar_product(forcing_term_tensor,
-                           fe_values[velocity].value(i, q)) *
-            fe_values.JxW(q);
-      }
-    }
-
-    // Boundary integral for Neumann BCs.
-    if (cell->at_boundary())
-    {
-      for (unsigned int f = 0; f < cell->n_faces(); ++f)
-      {
-        if (cell->face(f)->at_boundary() &&
-            cell->face(f)->boundary_id() == 2)
-        {
-          fe_face_values.reinit(cell, f);
-
-          for (unsigned int q = 0; q < n_q_face; ++q)
-          {
-            for (unsigned int i = 0; i < dofs_per_cell; ++i)
-            {
-              cell_rhs(i) +=
-                  -simulation_settings.outlet_pressure *
-                  scalar_product(fe_face_values.normal_vector(q),
-                                 fe_face_values[velocity].value(i,
-                                                                q)) *
-                  fe_face_values.JxW(q);
-            }
-          }
-        }
-      }
-    }
-
-    cell->get_dof_indices(dof_indices);
-
-    jacobian_matrix.add(dof_indices, cell_matrix);
-    residual_vector.add(dof_indices, cell_rhs);
-    pressure_mass.add(dof_indices, cell_pressure_mass_matrix);
-  }
-
-  jacobian_matrix.compress(VectorOperation::add);
-  residual_vector.compress(VectorOperation::add);
-  pressure_mass.compress(VectorOperation::add);
-
-  // Dirichlet boundary conditions.
-  {
-    std::map<types::global_dof_index, double> boundary_values;
-    std::map<types::boundary_id, const Function<dim> *> boundary_functions;
-
-    // We interpolate first the inlet velocity condition alone, then the wall
-    // condition alone, so that the latter "win" over the former where the two
-    // boundaries touch.
-    boundary_functions[1] = &inlet_velocity;
-    VectorTools::interpolate_boundary_values(dof_handler,
-                                             boundary_functions,
-                                             boundary_values,
-                                             ComponentMask(
-                                                 {true, true, true, false}));
-
-    boundary_functions.clear();
-    Functions::ZeroFunction<dim> zero_function(dim + 1);
-    boundary_functions[0] = &zero_function; // Body
-    boundary_functions[3] = &zero_function; // Slip
-    boundary_functions[4] = &zero_function; // Wall
-
-    VectorTools::interpolate_boundary_values(dof_handler,
-                                             boundary_functions,
-                                             boundary_values,
-                                             ComponentMask(
-                                                 {true, true, true, false}));
-
-    MatrixTools::apply_boundary_values(
-        boundary_values, jacobian_matrix, solution, residual_vector, false);
-  }
-}
 
 void Problem::assemble()
 {
@@ -509,7 +309,7 @@ void Problem::assemble()
                     fe_values[pressure].value(i, q) -
                 gamma *
                     velocity_divergence_loc *
-                    fe_values[velocity].divergence(i, q) - // Dovrebbe essere +?
+                    fe_values[velocity].divergence(i, q) -
                 scalar_product(forcing_term_tensor,
                                fe_values[velocity].value(i, q))) *
             fe_values.JxW(q);
@@ -693,7 +493,7 @@ void Problem::solveNewtonMethod()
     }
     else
     {
-      pcout << "Newton: residual norm was < tolerance" << std::endl;
+      pcout << " < tolerance" << std::endl;
     }
 
     ++n_iter;
